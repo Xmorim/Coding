f2026d0e233c53972f0c33764764ea143368e5c6
Mxrim <github@morim.me>
2025-10-29 15:32:03 -0300

    python progress

--- Python/easy.py
+++ Python/easy.py
@@ -5,6 +5,12 @@ def q2():
     name = str(input("name: "))
     print(f"Hello, {name}")
 
+def q3():
+    n1 = int(input("Primeiro número: "))
+    n2 = int(input("Segundo número: "))
+    s = n1 + n2
+
+    print(f"A soma dos números é: {s}")
 
 if __name__ == "__main__":
-    q2()
+    q3()
